61b50144b617622b274c1a1dcbb1a16930c4faf6
Jinjoo Park <jinjoo_park@naver.com>
2021-06-08 09:59:04 +0900

    웹폰트 비동기 테스트

---
+++ src/common/webfont.js
@@ -0,0 +1,15 @@
+WebFontConfig = {
+  custom: {
+      families: ['Spoqa Han Sans Neo'],
+      urls: ['/SpoqaHanSansNeo.css']
+  }
+};
+(function() {
+  var wf = document.createElement('script');
+  wf.src = ('https:' == document.location.protocol ? 'https' : 'http') +
+    '://ajax.googleapis.com/ajax/libs/webfont/1.4.10/webfont.js';
+  wf.type = 'text/javascript';
+  wf.async = 'true';
+  var s = document.getElementsByTagName('script')[0];
+  s.parentNode.insertBefore(wf, s);
+})(); 
